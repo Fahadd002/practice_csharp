1529e1842c2e3b542233375551bdd608723f94d3
fahadd002 <fpramanik6@gmail.com>
2025-09-27 02:25:06 +0600

    abstract class

---
+++ csharp_class/abrtuct_class/Product.cs
@@ -0,0 +1,35 @@
+﻿public abstract class Product
+{
+    public string Name { get; set; } = default!;
+    public decimal Price { get; set; }
+
+    public abstract decimal CalculateTotal();
+}
+
+public class Burger : Product
+{
+    public int Size { get; set; }
+    public string Flavour { get; set; } = default!;
+
+    public override decimal CalculateTotal()
+    {
+        if (Size > 5)
+            return Price * 1.2m;
+        else
+            return Price;
+    }
+}
+
+public class Camera : Product
+{
+    public int Pixel { get; set; }
+    public string Brand { get; set; } = default!;
+
+    public override decimal CalculateTotal()
+    {
+        if (Pixel > 20)
+            return Price + 200;
+        else
+            return Price;
+    }
+}
---
+++ csharp_class/abrtuct_class/Program.cs
@@ -0,0 +1,5 @@
+﻿Product burger = new Burger { Name = "Cheese Burger", Price = 5.00m, Size = 6, Flavour = "Beef" };
+Product camera = new Camera { Name = "DSLR Camera", Price = 1000.00m, Pixel = 24, Brand = "Canon" };
+
+Console.WriteLine($"{burger.Name} Total: {burger.CalculateTotal():C}");
+Console.WriteLine($"{camera.Name} Total: {camera.CalculateTotal():C}");
