a4185f93c6f73f2254ee201cfd3238647f08fe66
fahadd002 <fpramanik6@gmail.com>
2025-10-01 02:37:26 +0600

    generics.csproj

---
+++ csharp_class/generics/BubbleSorts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace generics
+{
+    public class BubbleSorts<T> where T : INumber<T>
+    {
+        public T[] Items { get; private set; }
+        public BubbleSorts(T[] items) 
+        {
+        Items = items;
+        }
+
+        public void Sort()
+        {
+            T temp;
+            for (int i = 0; i <= Items.Length - 2; i++)
+            {
+                for (int j = 0; j <= Items.Length -2; j++)
+                {
+                    if (Items[j] > Items[j + 1])
+                    {
+                        temp = Items[j + 1];
+                        Items[j + 1] = Items[j];
+                        Items[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ csharp_class/generics/Program.cs
@@ -0,0 +1,7 @@
+﻿using generics;
+
+BubbleSorts<double> bubbleSort = new BubbleSorts<double>([3.3, 1.2, 5.9, 7.7, 3.1, 4.6, 9.2]);
+bubbleSort.Sort();
+
+foreach (var item in bubbleSort.Items)
+    Console.WriteLine(item);
